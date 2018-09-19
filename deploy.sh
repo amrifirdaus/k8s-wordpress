c64af339b4cdb3be07ce2e56585ad99891a95d61
amrifirdaus <amrifirdaus>
2018-09-19 15:32:48 +0700

    Modify secret mysql configuration

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,14 @@
 #! /bin/bash
 
 echo 'Create secret'
-echo 'asd1234' > password.txt
-kubectl create secret generic mysql --from-file=password.txt
+kubectl create secret generic mysql --from-literal=password=asd1234
 echo
 echo 'Deploy MySQL'
 kubectl create -f mysql.yaml
 kubectl create -f mysql-service.yaml
 echo
 echo 'Waiting for mysql service ready'
-sleep 10m #wait 10 minutes for mysql ready
+sleep 5m #wait 5 minutes for mysql ready
 kubectl get service mysql
 
 echo 'Deploy and Expose WordPress'
@@ -17,6 +16,6 @@ kubectl create -f wordpress.yaml
 kubectl create -f wordpress-service.yaml
 echo
 echo 'Waiting for wordpress service ready'
-sleep 10m #wait 10 minutes for all pods ready
+sleep 5m #wait 5 minutes for all pods ready
 echo 'Check External IP'
 kubectl get svc -l app=wordpress
