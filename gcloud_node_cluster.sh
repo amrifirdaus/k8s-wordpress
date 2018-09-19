b1a8216b29147e746b8bfad615b4c6e5d2a940c7
amrifirdaus <amrifirdaus>
2018-09-19 15:31:17 +0700

    Change cluster name

--- gcloud_node_cluster.sh
+++ gcloud_node_cluster.sh
@@ -2,4 +2,4 @@
 echo 'Set config project, zone and create clusters'
 gcloud config set project training-216804
 gcloud config set compute/zone asia-southeast1-b
-gcloud container clusters create k8-wordpress --num-nodes 3 --machine-type g1-small
+gcloud container clusters create k8s-wordpress --num-nodes 3 --machine-type g1-small
