083bf3400b7e01bee2475a00df7750a7ac853d36
amrifirdaus <amrifirdaus>
2018-09-19 12:00:45 +0700

    Add sleep for waiting process

--- deploy.sh
+++ deploy.sh
@@ -3,14 +3,20 @@
 echo 'Create secret'
 echo 'asd1234' > password.txt
 kubectl create secret generic mysql --from-file=password.txt
-
+echo
 echo 'Deploy MySQL'
 kubectl create -f mysql.yaml
 kubectl create -f mysql-service.yaml
+echo
+echo 'Waiting for mysql service ready'
+sleep 10m #wait 10 minutes for mysql ready
+kubectl get service mysql
 
 echo 'Deploy and Expose WordPress'
 kubectl create -f wordpress.yaml
 kubectl create -f wordpress-service.yaml
-
+echo
+echo 'Waiting for wordpress service ready'
+sleep 10m #wait 10 minutes for all pods ready
 echo 'Check External IP'
 kubectl get svc -l app=wordpress
--- pv-claim.sh
+++ pv-claim.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
 kubectl apply -f mysql-pv-claim.yaml
+sleep 1m #waiting for pv mysql ready
+
 kubectl apply -f wordpress-pv-claim.yaml
+sleep 2m #waiting for pv wordpress ready
+
+echo 'Check PVC'
 kubectl get pvc
